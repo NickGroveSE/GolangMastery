f0798e09ef22dffdfbd99251b6de4e906008b267
Nick Grove <nickgrove.se@gmail.com>
2024-01-20 13:36:15 -0500

    Performing Scrape, Some More Changes Needed Though

--- 01_WebScraper/webscraper.go
+++ 01_WebScraper/webscraper.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+
+	// filmNames := []string{}
+
+	fmt.Print("Instantiating...\n")
+	// Instantiate default collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("letterboxd.com"),
+	)
+
+	c.OnHTML(".image", func(e *colly.HTMLElement) {
+		// film := e.Attr("data-film-name")
+		// filmNames = append(filmNames, film)
+		fmt.Println(e.Attr("alt"))
+	})
+
+	// c.OnScraped(func(r *colly.Response) {
+	// 	fmt.Println(filmNames)
+	// })
+
+	c.Visit("https://letterboxd.com/feauv/")
+
 }
