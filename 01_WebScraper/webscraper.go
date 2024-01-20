9808421a6d52f9fa45b0fc3692f64d585659acba
Nick Grove <nickgrove.se@gmail.com>
2024-01-20 15:29:12 -0500

    Web Scraper Fully Finished

--- 01_WebScraper/webscraper.go
+++ 01_WebScraper/webscraper.go
@@ -2,36 +2,67 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
 
-	// filmNames := []string{}
+	favorites := []string{}
+	recents := []string{}
+	ratings := []string{}
 
 	fmt.Print("Instantiating...\n\n")
 
-	// fmt.Print("Favorites:\n")
 	// Instantiate default collector
 	c := colly.NewCollector(
 		colly.AllowedDomains("letterboxd.com"),
 	)
 
 	c.OnHTML("#favourites", func(e *colly.HTMLElement) {
-		// film := e.Attr("data-film-name")
-		// filmNames = append(filmNames, film)
-
-		for _, film := range e.ChildAttrs("ul > li > div.poster > img", "alt") {
-			fmt.Println("\t" + film)
-		}
+		favorites = e.ChildAttrs("ul > li > div.poster > img", "alt")
 
 	})
 
-	// c.OnScraped(func(r *colly.Response) {
-	// 	fmt.Println(filmNames)
-	// })
+	c.OnHTML("#recent-activity", func(e *colly.HTMLElement) {
+		recents = e.ChildAttrs("ul > li > div.poster > img", "alt")
+		ratings = strings.Split(e.ChildText("ul > li > p > span"), " ")
+	})
 
-	c.Visit("https://letterboxd.com/feauv/")
+	c.OnScraped(func(r *colly.Response) {
+
+		printFaves(favorites)
+
+		printRecents(recents, ratings)
+
+		fmt.Println(ratings[1])
+
+	})
+
+	c.Visit("https://letterboxd.com/filmarchive/")
+
+}
+
+func printFaves(films []string) {
+
+	fmt.Print("Favorite Films:\n")
+
+	for _, film := range films {
+		fmt.Println("\t" + film)
+	}
+
+	fmt.Print("\n")
+}
+
+func printRecents(films []string, ratings []string) {
+
+	fmt.Print("Recent Films:\n")
+
+	for index, film := range films {
+		fmt.Println("\t" + film + " - " + ratings[index+index])
+	}
+
+	fmt.Print("\n")
 
 }
